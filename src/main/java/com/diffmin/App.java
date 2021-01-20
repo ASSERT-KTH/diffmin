d11b908a686346b84f429728f1ec889833f93b0e
Aman Sharma <mannu.poski10@gmail.com>
2021-01-20 18:34:26 +0530

    Add function to get all operations
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/main/java/com/diffmin/App.java
+++ src/main/java/com/diffmin/App.java
@@ -1,27 +1,41 @@
 package com.diffmin;
 
 import java.io.File;
-import gumtree.spoon.AstComparator;
-import gumtree.spoon.diff.Diff;
+import java.util.List;
 
+import gumtree.spoon.AstComparator;
+import gumtree.spoon.diff.operations.Operation;
 
 public class App
 {
     public static String sample(String sampleString) {
         return sampleString;
     }
+
+    public static List<Operation> getOperations(File f1, File f2) throws Exception {
+        final List<Operation> operations;
+        try {
+            operations = new AstComparator().compare(f1, f2).getRootOperations();
+        } catch (Exception e) {
+            throw e;
+        }
+        return operations;
+    }
+
     public static void main( String[] args ) {
         if (args.length != 2) {
             System.out.println("Usage: DiffSpoon <file_1>  <file_2>");
             return;
         }
-        final Diff result;
+        List<Operation> operations;
         try {
-            result = new AstComparator().compare(new File(args[0]), new File(args[1]));
-            System.out.println(result.toString());
+            operations = App.getOperations(new File(args[0]), new File(args[1]));
+            for (int op=0; op<operations.size(); ++op) {
+
+                System.out.println(operations.get(op).toString());
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println(e);
         }
     }
 }
