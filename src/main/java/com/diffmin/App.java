70c03c2784814fcc15dd06a75017186836fd8b32
Aman Sharma <mannu.poski10@gmail.com>
2021-01-20 08:08:50 +0530

    Integrate local build of gumtree-spoon-ast-diff
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/main/java/com/diffmin/App.java
+++ src/main/java/com/diffmin/App.java
@@ -1,16 +1,27 @@
 package com.diffmin;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+import gumtree.spoon.AstComparator;
+import gumtree.spoon.diff.Diff;
+
+
 public class App
 {
     public static String sample(String sampleString) {
         return sampleString;
     }
-    public static void main( String[] args )
-    {
-        System.out.println("Hello World!");
+    public static void main( String[] args ) {
+        if (args.length != 2) {
+            System.out.println("Usage: DiffSpoon <file_1>  <file_2>");
+            return;
+        }
+        final Diff result;
+        try {
+            result = new AstComparator().compare(new File(args[0]), new File(args[1]));
+            System.out.println(result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
     }
 }
