ce9670fd073860fb740a0b8d196411ddc5f937a7
Aman Sharma <mannu.poski10@gmail.com>
2021-01-28 23:55:29 +0530

    Add function to update tree in case of Update operation
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/main/java/com/diffmin/App.java
+++ src/main/java/com/diffmin/App.java
@@ -1,10 +1,15 @@
 package com.diffmin;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 
+import com.github.gumtreediff.tree.ITree;
+import com.github.gumtreediff.tree.TreeUtils;
 import gumtree.spoon.AstComparator;
 import gumtree.spoon.diff.operations.Operation;
+import gumtree.spoon.builder.SpoonGumTreeBuilder;
+import spoon.reflect.declaration.CtElement;
 
 public class App
 {
@@ -22,18 +27,37 @@ public class App
         return operations;
     }
 
+    public static ITree patch(File f1, List<Operation> operations) throws Exception {
+        final SpoonGumTreeBuilder scanner = new SpoonGumTreeBuilder();
+        CtElement prevFile = new AstComparator().getCtType(f1);
+        ITree prevTree = scanner.getTree(prevFile);
+        for (int i=0; i<operations.size(); ++i) {
+            Operation operation = operations.get(i);
+            // Currently only update operation is handled
+            ITree updatedNode = (ITree)operation.getDstNode().getMetadata(SpoonGumTreeBuilder.GUMTREE_NODE);
+            List<ITree> bfsDst = TreeUtils.breadthFirst(prevTree);
+            Iterator it = bfsDst.iterator();
+            while (it.hasNext()) {
+                ITree x = (ITree) it.next();
+                if (x.getType() == updatedNode.getType()) {
+                    x.setLabel(updatedNode.getLabel());
+                }
+            }
+        }
+        return prevTree;
+    }
+
     public static void main( String[] args ) {
         if (args.length != 2) {
             System.out.println("Usage: DiffSpoon <file_1>  <file_2>");
             return;
         }
         List<Operation> operations;
+        ITree patchedTree;
         try {
             operations = App.getOperations(new File(args[0]), new File(args[1]));
-            for (int op=0; op<operations.size(); ++op) {
-
-                System.out.println(operations.get(op).toString());
-            }
+            patchedTree = App.patch(new File(args[0]), operations);
+            System.out.println(patchedTree.toTreeString());
         } catch (Exception e) {
             e.printStackTrace();
         }
