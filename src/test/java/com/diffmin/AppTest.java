ad339a0ed9ec45fa471fafec188911e36b06d6ee
Aman Sharma <mannu.poski10@gmail.com>
2021-02-15 20:33:20 +0530

    Add test to check if the literal is replaced
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/test/java/com/diffmin/AppTest.java
+++ src/test/java/com/diffmin/AppTest.java
@@ -1,9 +1,15 @@
 package com.diffmin;
 
 import com.diffmin.App;
-import static org.junit.Assert.assertTrue;
 
+import gumtree.spoon.AstComparator;
 import org.junit.Test;
+import spoon.reflect.declaration.CtElement;
+import gumtree.spoon.diff.operations.Operation;
+
+
+import java.io.File;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -14,10 +20,12 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        String sampleString = "Hello World!";
-        String test = App.sample(sampleString);
-        assert test == sampleString;
+    public void prevFileShouldBecomeNewFile() throws Exception {
+        File f1 = new File("src/test/resources/action1.java");
+        File f2 = new File("src/test/resources/action2.java");
+        CtElement c2 = new AstComparator().getCtType(f2);
+        List<Operation> operations = App.getOperations(f1, f2);
+        CtElement patchedCtElement = App.patch(f1, operations);
+        assert patchedCtElement.prettyprint().equals(c2.prettyprint());
     }
 }
