8ff0d9e661e58b07bf09583f40e7978164bc36f1
Aman Sharma <mannu.poski10@gmail.com>
2021-02-17 14:38:52 +0530

    Use replace node while updating invocation
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/main/java/com/diffmin/UpdateInvocation.java
+++ src/main/java/com/diffmin/UpdateInvocation.java
@@ -1,12 +1,9 @@
 package com.diffmin;
 
-import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtInvocation;
 
 public class UpdateInvocation {
     public static void process(CtInvocation prevNode, CtInvocation newNode) {
-        CtBlock block = (CtBlock) prevNode.getParent();
-        block.insertBegin(newNode.clone());
-        prevNode.delete();
+        prevNode.replace(newNode);
     }
 }
