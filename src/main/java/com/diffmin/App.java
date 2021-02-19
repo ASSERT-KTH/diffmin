6d88bcb90e12509ca535bcba6ef2798cd4ae968a
Aman Sharma <mannu.poski10@gmail.com>
2021-02-19 14:54:04 +0530

    Reformat code according to the configurations set up
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/main/java/com/diffmin/App.java
+++ src/main/java/com/diffmin/App.java
@@ -3,27 +3,40 @@ package com.diffmin;
 import com.github.gumtreediff.actions.model.Update;
 import gumtree.spoon.AstComparator;
 import gumtree.spoon.diff.operations.Operation;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 import spoon.reflect.declaration.CtElement;
 
-import java.io.File;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Collections;
-import java.util.Iterator;
+/**
+ * Entry point of the project. Computes the edit script and uses it to patch the.
+ */
+public class App {
 
-public class App
-{
+    /**
+     * Computes the list of operations which is used for patching `f2`.
+     *
+     * @param f1 Previous version of the file
+     * @param f2 Modified version of the file
+     * @return List of operations in the edit script
+     * @throws Exception Exception raised via {@link AstComparator}
+     */
     public static List<Operation> getOperations(File f1, File f2) throws Exception {
         List<Operation> rootOperations =  new AstComparator().compare(f1, f2).getRootOperations();
         List<Operation> mutableList = new ArrayList<>(rootOperations);
 
         class OperationSorter implements Comparator<Operation> {
             /**
+             * Sorts the list of operation according to the sizes of nodes attached to each action
+             * in descending order.
              *
              * @param o1 First list item
              * @param o2 Second list item
-             * @return [-1,0,1] result of comparison of sum of sizes of children list of src and dest node
+             * @return [-1,0,1] result of comparison of sum of sizes of children list of src and
+             * dest node
              */
             @Override
             public int compare(Operation o1, Operation o2) {
@@ -31,7 +44,9 @@ public class App
                 int o2SrcChildren = o2.getSrcNode().getDirectChildren().size();
                 int o1DestChildren = o1.getDstNode().getDirectChildren().size();
                 int o2DestChildren = o2.getDstNode().getDirectChildren().size();
-                return Double.compare(o1SrcChildren + o1DestChildren, o2SrcChildren + o2DestChildren);
+                return Double.compare(
+                        o1SrcChildren + o1DestChildren, o2SrcChildren + o2DestChildren
+                );
             }
         }
 
@@ -41,6 +56,14 @@ public class App
         return mutableList;
     }
 
+    /**
+     * Patches `f1` using the edit script.
+     *
+     * @param f1 Previous version of the file which needs to be modified
+     * @param operations List of operations which will govern how `f1` will be patched
+     * @return Updated {@link CtElement} of `f1`
+     * @throws Exception Exception raised via {@link AstComparator}
+     */
     public static CtElement patch(File f1, List<Operation> operations) throws Exception {
         CtElement prevFileElement = new AstComparator().getCtType(f1);
         for (Operation operation : operations) {
@@ -60,7 +83,12 @@ public class App
         return prevFileElement;
     }
 
-    public static void main( String[] args ) {
+    /**
+     * Runs the patch function and dumps the output in the terminal.
+     *
+     * @param args Arguments passed via command line
+     */
+    public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: DiffSpoon <file_1>  <file_2>");
             return;
--- src/test/java/com/diffmin/AppTest.java
+++ src/test/java/com/diffmin/AppTest.java
@@ -1,26 +1,34 @@
 package com.diffmin;
 
+import gumtree.spoon.AstComparator;
+import gumtree.spoon.diff.operations.Operation;
+import java.io.File;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-
-import gumtree.spoon.AstComparator;
 import org.junit.jupiter.params.provider.MethodSource;
 import spoon.reflect.declaration.CtElement;
-import gumtree.spoon.diff.operations.Operation;
-
-import java.io.File;
-import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
+    /**
+     * Tests for verifying if update patch works correctly.
+     *
+     * @param testMessage  Test message to be displayed
+     * @param filePathPrev File path of previous version
+     * @param filePathNew  File path of modified version
+     * @throws Exception Exception raised via {@link AstComparator}, {@link App} and,
+     * {@link AssertionError}
+     */
     @DisplayName("Should patch old file with Update operations")
-    @ParameterizedTest(name="{0}")
+    @ParameterizedTest(name = "{0}")
     @MethodSource("resourceProvider")
-    public void shouldUpdate( String testMessage, String filePathPrev, String filePathNew) throws Exception {
+    public void shouldUpdate(
+            String testMessage, String filePathPrev, String filePathNew
+    ) throws Exception {
         File f1 = new File(filePathPrev);
         File f2 = new File(filePathNew);
         CtElement expectedNewElement = new AstComparator().getCtType(f2);
@@ -31,10 +39,25 @@ public class AppTest
 
     private static Arguments[] resourceProvider() {
         return new Arguments[]{
-            Arguments.of("Should update literal", "src/test/resources/update/literal/prev.java", "src/test/resources/update/literal/new.java"),
-            Arguments.of("Should update invocation", "src/test/resources/update/invocation/prev.java", "src/test/resources/update/invocation/new.java"),
-            Arguments.of("Should update literal and invocation", "src/test/resources/update/literal/prev.java", "src/test/resources/update/literal/new.java"),
-            Arguments.of("Should update type reference", "src/test/resources/update/typeref/prev.java", "src/test/resources/update/typeref/new.java"),
+            Arguments.of(
+                    "Should update literal",
+                    "src/test/resources/update/literal/prev.java",
+                    "src/test/resources/update/literal/new.java"
+            ),
+            Arguments.of(
+                    "Should update invocation",
+                    "src/test/resources/update/invocation/prev.java",
+                    "src/test/resources/update/invocation/new.java"
+            ),
+            Arguments.of(
+                    "Should update literal and invocation",
+                    "src/test/resources/update/literal+invocation/prev.java",
+                    "src/test/resources/update/literal+invocation/new.java"
+            ),
+            Arguments.of("Should update type reference",
+                    "src/test/resources/update/typeref/prev.java",
+                    "src/test/resources/update/typeref/new.java"
+            ),
         };
     }
 }
