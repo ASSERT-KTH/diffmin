cee78bec26e59de7bb0028e62709fbae652b6f27
Aman Sharma <mannu.poski10@gmail.com>
2021-04-01 03:07:47 +0530

    Refactor test to pick source files directly from walking the directories
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/test/java/com/diffmin/AppTest.java
+++ src/test/java/com/diffmin/AppTest.java
@@ -2,15 +2,20 @@ package com.diffmin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import gumtree.spoon.AstComparator;
 import gumtree.spoon.diff.operations.Operation;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import org.junit.jupiter.api.DisplayName;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import spoon.Launcher;
 import spoon.reflect.CtModel;
 import spoon.reflect.declaration.CtCompilationUnit;
@@ -20,25 +25,120 @@ import spoon.reflect.declaration.CtType;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Tests for verifying if patch works correctly.
-     *
-     * @param testMessage  Test message to be displayed
-     * @param prevFilePath File path of previous version
-     * @param nextFilePath File path of modified version
-     * @throws Exception Exception raised via {@link AstComparator}, {@link App} and,
-     * {@link AssertionError}
-     */
-    @DisplayName("Should patch old file with operations")
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("resourceProvider")
-    public void shouldPatch(
-            String testMessage, String prevFilePath, String nextFilePath
-    ) throws Exception {
-        File f1 = new File(prevFilePath);
-        File f2 = new File(nextFilePath);
 
-        App app = new App(prevFilePath);
+    public static final Path RESOURCES_BASE_DIR = Paths.get("src/test/resources");
+    public static final Path PURE_DELETE_PATCHES = RESOURCES_BASE_DIR.resolve("delete");
+    public static final Path PURE_UPDATE_PATCHES = RESOURCES_BASE_DIR.resolve("update");
+    public static final Path DELETE_UPDATE_PATCHES = RESOURCES_BASE_DIR.resolve("delete+update");
+
+    private static Stream<? extends Arguments> getArgumentSourceStream(
+            File testDir, Function<File, TestResources> sourceGetter) {
+        return Arrays.stream(testDir.listFiles())
+                .filter(File::isDirectory)
+                .map(sourceGetter)
+                .map(Arguments::of);
+    }
+
+    /**
+     * Class to provide test resources.
+     */
+    public static class TestResources {
+        public String parent;
+        public Path prevPath;
+        public Path newPath; // stylised new
+
+        /**
+         * Constructor of {@link TestResources}.
+         *
+         * @param prevPath path of the previous version of a file
+         * @param newPath path of the new version of a file
+         * @param parent name of the directory containing the two files
+         */
+        TestResources(Path prevPath, Path newPath, String parent) {
+            this.prevPath = prevPath;
+            this.newPath = newPath;
+            this.parent = parent;
+        }
+
+        /**
+         * Resolve files inside a directory.
+         *
+         * @param testDir Directory containing test files
+         * @return instance of {@link TestResources}
+         */
+        public static TestResources fromTestDirectory(File testDir) {
+            Path path = testDir.toPath();
+            String parent = testDir.getName();
+            return new TestResources(
+                    path.resolve("prev.java"),
+                    path.resolve("new.java"),
+                    parent
+            );
+        }
+
+        @Override
+        public String toString() {
+            return parent;
+        }
+    }
+
+    /** Provides test sources for scenarios where only update patches are applied. */
+    public static class PureUpdatePatches implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+            return getArgumentSourceStream(
+                    PURE_UPDATE_PATCHES.toFile(),
+                    TestResources::fromTestDirectory
+            );
+        }
+    }
+
+    /** Provides test sources for scenarios where only delete patches are applied. */
+    public static class PureDeletePatches implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+            return getArgumentSourceStream(
+                    PURE_DELETE_PATCHES.toFile(),
+                    TestResources::fromTestDirectory
+            );
+        }
+    }
+
+    /** Provides test sources for scenarios where only delete patches are applied. */
+    public static class DeleteUpdatePatches implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+            return getArgumentSourceStream(
+                    DELETE_UPDATE_PATCHES.toFile(),
+                    TestResources::fromTestDirectory
+            );
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(PureUpdatePatches.class)
+    void should_apply_pure_update_patches(TestResources sources) throws Exception {
+        runTests(sources);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(PureDeletePatches.class)
+    void should_apply_pure_delete_patches(TestResources sources) throws Exception {
+        runTests(sources);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DeleteUpdatePatches.class)
+    void should_apply_delete_update_patches(TestResources sources) throws Exception {
+        runTests(sources);
+    }
+
+    private static void runTests(TestResources sources) throws Exception {
+        File f1 = sources.prevPath.toFile();
+        File f2 = sources.newPath.toFile();
+
+        App app = new App(sources.prevPath.toString());
+
         List<Operation> operations = app.getOperations(f1, f2);
         app.generatePatch(operations);
         app.applyPatch();
@@ -46,80 +146,28 @@ public class AppTest {
 
         final Launcher launcher = new Launcher();
         launcher.getEnvironment().setCommentEnabled(false);
-        launcher.addInputResource(nextFilePath);
+        launcher.addInputResource(sources.newPath.toString());
         CtModel expectedModel = launcher.buildModel();
         Optional<CtType<?>> firstType = expectedModel.getAllTypes().stream().findFirst();
 
         if (firstType.equals(Optional.empty())) {
             assertEquals(
-                patchedCtModel.getAllTypes().stream().findFirst(),
-                Optional.empty(),
-                "Patched prev file is not empty"
+                    patchedCtModel.getAllTypes().stream().findFirst(),
+                    Optional.empty(),
+                    "Patched prev file is not empty"
 
             );
         }
         else {
             CtType<?> retrievedFirstType = firstType.get();
             CtCompilationUnit cu = retrievedFirstType
-                .getFactory().CompilationUnit().getOrCreate(retrievedFirstType);
+                    .getFactory().CompilationUnit().getOrCreate(retrievedFirstType);
             String patchedProgram = app.displayModifiedModel(patchedCtModel);
             assertEquals(
-                cu.prettyprint(),
-                patchedProgram,
-                "Prev file was not patched correctly"
+                    cu.prettyprint(),
+                    patchedProgram,
+                    "Prev file was not patched correctly"
             );
         }
-    }
-
-    private static Arguments[] resourceProvider() {
-        return new Arguments[]{
-            // Pure delete patches
-            Arguments.of(
-                "Should delete literal",
-                "src/test/resources/delete/literal/left.java",
-                "src/test/resources/delete/literal/right.java"
-            ),
-            Arguments.of(
-                "Should delete a specific literal",
-                "src/test/resources/delete/specific_literal/left.java",
-                "src/test/resources/delete/specific_literal/right.java"
-            ),
-            Arguments.of(
-                "Should delete an entire program",
-                "src/test/resources/delete/entire_file/left.java",
-                "src/test/resources/delete/entire_file/right.java"
-            ),
-            // Pure update patches
-            Arguments.of(
-                "Should update invocation",
-                "src/test/resources/update/invocation/prev.java",
-                "src/test/resources/update/invocation/new.java"
-            ),
-            Arguments.of(
-                "Should update literal",
-                "src/test/resources/update/literal/prev.java",
-                "src/test/resources/update/literal/new.java"
-            ),
-            Arguments.of(
-                "Should update literal and invocation",
-                "src/test/resources/update/literal/prev.java",
-                "src/test/resources/update/literal/new.java"
-            ),
-            Arguments.of(
-                "Should update type reference",
-                "src/test/resources/update/typeref/prev.java",
-                "src/test/resources/update/typeref/new.java"
-            ),
-            Arguments.of(
-                "Should preserve formatting and update local variable",
-                "src/test/resources/update/formatting_change/prev.java",
-                "src/test/resources/update/formatting_change/new.java"
-            ),
-            Arguments.of(
-                "Should apply multiple update patches",
-                "src/test/resources/update/multiple_updates/prev.java",
-                "src/test/resources/update/multiple_updates/new.java"
-            )
-        };
     }
 }
