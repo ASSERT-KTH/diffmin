72b2e1d356d33feecf2a24391efa0e6d416a3a86
Aman Sharma <mannu.poski10@gmail.com>
2021-04-01 22:01:49 +0530

    Modify Pair util to use getters
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/main/java/com/diffmin/util/Pair.java
+++ src/main/java/com/diffmin/util/Pair.java
@@ -7,17 +7,35 @@ package com.diffmin.util;
  * @param <P> Generic type for second element inside Pair
  */
 public class Pair<T, P> {
-    public final T a;
-    public final P b;
+    private final T first;
+    private final P second;
 
     /**
      * Constructs a Pair instance.
      *
-     * @param a first element
-     * @param b second element
+     * @param first first element
+     * @param second second element
      */
-    public Pair(T a, P b) {
-        this.a = a;
-        this.b = b;
+    public Pair(T first, P second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Returns the first element.
+     *
+     * @return the first element
+     */
+    public T getFirst() {
+        return this.first;
+    }
+
+    /**
+     * Returns the second element.
+     *
+     * @return the second element
+     */
+    public P getSecond() {
+        return this.second;
     }
 }
