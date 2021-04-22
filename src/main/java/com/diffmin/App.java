a740dbd0e2b681d1c9e1737ab716d1cfdb1a7988
Aman Sharma <mannu.poski10@gmail.com>
2021-04-22 18:40:56 +0530

    Upgrade function of applying insert patches by making it more precise (#22)

--- src/main/java/com/diffmin/App.java
+++ src/main/java/com/diffmin/App.java
@@ -10,15 +10,25 @@ import gumtree.spoon.diff.operations.OperationKind;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import spoon.Launcher;
 import spoon.compiler.SpoonResource;
 import spoon.compiler.SpoonResourceHelper;
 import spoon.reflect.CtModel;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.code.CtStatement;
+import spoon.reflect.code.CtStatementList;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtCompilationUnit;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtPackage;
 import spoon.reflect.declaration.CtType;
+import spoon.reflect.declaration.CtTypeMember;
 
 /**
  * Entry point of the project. Computes the edit script and uses it to patch the.
@@ -27,7 +37,7 @@ public class App {
 
     private List<CtElement> deletePatches = new ArrayList<>();
     private List<Pair<CtElement, CtElement>> updatePatches = new ArrayList<>();
-    private List<Pair<CtElement, CtElement>> insertPatches = new ArrayList<>();
+    private Set<ImmutableTriple<Integer, CtElement, CtElement>> insertPatches = new HashSet<>();
     public CtModel modelToBeModified;
 
     /**
@@ -81,6 +91,27 @@ public class App {
     }
 
     /**
+     * Returns the corresponding list of elements in parent.
+     *
+     * @param element the element whose parent is a collection
+     * @return the list of entities the `element`'s parent contains
+     */
+    private List<? extends CtElement> getCollectionElementList(CtElement element) {
+        switch (element.getRoleInParent()) {
+            case STATEMENT:
+                return ((CtStatementList) element.getParent()).getStatements();
+            case ARGUMENT:
+                return ((CtInvocation<?>) element.getParent()).getArguments();
+            case TYPE_MEMBER:
+                return ((CtClass<?>) element.getParent()).getTypeMembers();
+            default:
+                throw new UnsupportedOperationException(
+                        "Unsupported role: " + element.getRoleInParent()
+                );
+        }
+    }
+
+    /**
      * Pretty prints the model.
      *
      * @param model model to be pretty printed
@@ -116,10 +147,20 @@ public class App {
             else if (operation.getAction() instanceof Insert) {
                 CtElement srcNode = operation.getSrcNode();
                 CtElement srcNodeParent = srcNode.getParent();
-                List<CtElement> elementsToBeInserted = getElementToBeModified(srcNodeParent);
-                for (CtElement ctElement : elementsToBeInserted) {
-                    insertPatches.add(new Pair<>(ctElement, srcNodeParent));
-                }
+                List<? extends CtElement> newCollectionList =
+                        getCollectionElementList(srcNode);
+                // Assuming only one element will be matched in the previous model.
+                CtElement parentElementInPrevModel =
+                        getElementToBeModified(srcNodeParent).get(0);
+
+                int srcNodeIndex = IntStream.range(0, newCollectionList.size())
+                        .filter(i -> newCollectionList.get(i) == srcNode)
+                        .findFirst()
+                        .getAsInt();
+
+                insertPatches.add(
+                        new ImmutableTriple<>(srcNodeIndex, srcNode, parentElementInPrevModel)
+                );
             }
         }
     }
@@ -136,10 +177,46 @@ public class App {
             CtElement newNode = update.getSecond();
             prevNode.replace(newNode);
         }
-        for (Pair<CtElement, CtElement> patch : insertPatches) {
-            CtElement prevNode = patch.getFirst();
-            CtElement newNode = patch.getSecond();
-            prevNode.replace(newNode);
+        insertPatches.forEach(App::applyInsertion);
+    }
+
+    /** Apply the insert patch. */
+    private static void applyInsertion(ImmutableTriple<Integer, CtElement, CtElement> insert) {
+        int where = insert.left;
+        CtElement toBeInserted = insert.middle;
+        CtElement inWhichElement = insert.right;
+
+        switch (toBeInserted.getRoleInParent()) {
+            case STATEMENT:
+                ((CtStatementList) inWhichElement)
+                        .addStatement(where, (CtStatement) toBeInserted.clone());
+                break;
+            // FIXME temporary workaround until INRIA/spoon#3885 is merged
+            case ARGUMENT:
+                List<CtExpression<?>> arguments =
+                        ((CtInvocation<?>) inWhichElement).getArguments();
+                // If size of the arguments list is 0, we cannot add an argument to it because
+                // when it is empty, it is an instance of EmptyClearableList. Thus, instead, we
+                // use the `addArgument` API.
+                if (arguments.isEmpty()) {
+                    ((CtInvocation<?>) inWhichElement)
+                            .addArgument((CtExpression<?>) toBeInserted);
+                }
+                // `addArgument` API cannot be used here as it only appends the new argument
+                // and we cannot do precise insertions by providing the index. But a non-empty
+                // argument list is an instance of ModelList which can be mutated.
+                else {
+                    arguments.add(where, (CtExpression<?>) toBeInserted);
+                }
+                break;
+            case TYPE_MEMBER:
+                ((CtClass<?>) inWhichElement)
+                        .addTypeMemberAt(where, (CtTypeMember) toBeInserted);
+                break;
+            default:
+                throw new UnsupportedOperationException(
+                        "Unhandled role: " + toBeInserted.getRoleInParent()
+                );
         }
     }
 
