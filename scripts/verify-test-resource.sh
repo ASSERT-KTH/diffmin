11f63b355b1518134c4fffec1691633a63f95f78
Aman Sharma <mannu.poski10@gmail.com>
2021-05-04 13:26:07 +0530

    Exit script with code 1 if atleast one java file does not compile
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- scripts/verify-test-resource.sh
+++ scripts/verify-test-resource.sh
@@ -15,32 +15,51 @@ TEST_RESOURCES_PATH=src/test/resources
 
 # Temporary directory for saving compiled classes
 TEMP_DIR_COMPILED_RESOURCES=$(mktemp -d)
-echo "Created $TEMP_DIR_COMPILED_RESOURCES for saving compiled files."
+echo "Created $TEMP_DIR_COMPILED_RESOURCES for saving compiled files temporarily."
 echo -e "It will be removed after the script has executed with any exit code.\n"
 
 # Delete the temporary directory as it served its purpose
 cleanup() {
   echo -e "\nRemoving $TEMP_DIR_COMPILED_RESOURCES directory ..."
   rm -rf "$TEMP_DIR_COMPILED_RESOURCES"
-  echo "Done, script execution complete."
+  echo "Done."
 }
 
 # Make sure the temporary directory is deleted even if the script aborts
 trap cleanup EXIT
 
+# Global variables which will be modified as the script runs
+status=0
+file_count=0
+compilation_errors=0
+
 # Verify if the resources compiles
 compile() {
   local file=$1
-  if javac "$file" -d "$TEMP_DIR_COMPILED_RESOURCES"
-    then
-      echo -e "${file}:${GREEN} Compiled successfully!${NC}"
-    else
-      echo -e "${RED}There were compilation errors in ${WHITE}${RED_BG}${file}${NC}"
+  if javac "$file" -d "$TEMP_DIR_COMPILED_RESOURCES"; then
+    echo -e "${file}:${GREEN} Compiled successfully!${NC}"
+  else
+    echo -e "${RED}There were compilation errors in ${WHITE}${RED_BG}${file}${NC}"
+    status=1
+    compilation_errors=$((compilation_errors+1))
   fi
 }
 
+# Record changes to global variables
+shopt -s lastpipe
+
 # Find all Java files inside the test resources directory
-find $TEST_RESOURCES_PATH -type f -name "*.java" | while read -r file
-  do
-    compile "$file"
-  done
+find $TEST_RESOURCES_PATH -type f -name "*.java" | while read -r file; do
+  compile "$file"
+  file_count=$((file_count+1))
+done
+
+if [[ $status -eq 0 ]]
+  then
+    echo -e "\n${GREEN}All [${file_count}/${file_count}] files were compiled successfully!${NC}"
+  else
+    echo -e "\n${RED}Errors in ${compilation_errors}/${file_count} files.${NC}"
+fi
+
+echo "Script exiting with code $status."
+exit $status
