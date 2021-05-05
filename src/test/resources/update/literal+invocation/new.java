67a4779e60fa42dc1c6cdf88a7052b99c2cab293
Aman Sharma <mannu.poski10@gmail.com>
2021-05-05 23:10:52 +0530

    Use System invocations to pass the tests
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/test/resources/update/literal+invocation/new.java
+++ src/test/resources/update/literal+invocation/new.java
@@ -1,8 +1,6 @@
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 class Assert {
     public static void main(String[] args) {
         int test = 1;
-        assertNull(test);
+        System.out.println(test);
     }
 }
--- src/test/resources/update/literal+invocation/prev.java
+++ src/test/resources/update/literal+invocation/prev.java
@@ -1,8 +1,6 @@
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 class Assert {
     public static void main(String[] args) {
         int test = 0;
-        assertNotNull(test);
+        System.out.print(test);
     }
 }
