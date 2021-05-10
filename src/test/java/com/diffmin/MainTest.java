03106107a3d26969c58f44d3f7c8839eb8cf554d
Aman Sharma <mannu.poski10@gmail.com>
2021-05-10 15:27:28 +0530

    Fix minor changes
    * Change access modifier
    * Move tear down method in proximity of set up method
    * Use assertThrows instead of try-catch
    
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/test/java/com/diffmin/MainTest.java
+++ src/test/java/com/diffmin/MainTest.java
@@ -10,11 +10,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /** Unit test to verify Main method's functioning */
-public class MainTest {
+class MainTest {
     /** Overrides the default security manager before starting execution of test cases. */
     @BeforeAll
     static void beforeAll() {
         System.setSecurityManager(new DoNotExitJVM());
+    }
+
+    /** Restores the default security manager after executing all test cases. */
+    @AfterAll
+    static void afterAll() {
+        System.setSecurityManager(null);
     }
 
     private static class ExitException extends SecurityException {
@@ -49,19 +55,14 @@ public class MainTest {
 
     @Test
     @DisplayName("should exit with code 1 when incorrect number of parameters are supplied")
-    public void should_exit_1_if_wrong_number_of_parameters_are_supplied() {
-        try {
-            Main.main(new String[] {});
-        } catch (ExitException e) {
-            assertEquals(1, e.status);
-        } catch (FileNotFoundException e) {
-            fail("File path could not found");
-        }
+    void should_exit_1_if_wrong_number_of_parameters_are_supplied() {
+        ExitException exec = assertThrows(ExitException.class, () -> Main.main(new String[] {}));
+        assertEquals(1, exec.status);
     }
 
     @Test
     @DisplayName("should throw FileNotFoundException if incorrect path is supplied")
-    public void should_throw_FileNotFoundException_if_path_is_incorrect() {
+    void should_throw_FileNotFoundException_if_path_is_incorrect() {
         String prevFile = "wrong/path/to/prevFile";
         String newFile = "wrong/path/to/newFile";
         assertThrows(
@@ -70,21 +71,13 @@ public class MainTest {
 
     @Test
     @DisplayName("should exit with code 0 when two correct paths are supplied")
-    public void should_exit_0_when_2_correct_paths_are_supplied() {
+    void should_exit_0_when_2_correct_paths_are_supplied() {
         String prevFile = "src/test/resources/delete/literal/PREV_DeleteLiteral.java";
         String newFile = "src/test/resources/delete/literal/NEW_DeleteLiteral.java";
-        try {
-            Main.main(new String[] {prevFile, newFile});
-        } catch (ExitException e) {
-            assertEquals(0, e.status);
-        } catch (FileNotFoundException e) {
-            fail("File path could not be found");
-        }
-    }
 
-    /** Restores the default security manager after executing all test cases. */
-    @AfterAll
-    static void afterAll() {
-        System.setSecurityManager(null);
+        ExitException exec =
+                assertThrows(
+                        ExitException.class, () -> Main.main(new String[] {prevFile, newFile}));
+        assertEquals(0, exec.status);
     }
 }
