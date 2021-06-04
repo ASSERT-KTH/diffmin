8682f13cfbc6d04a0c71420a5179d318206b1838
Aman Sharma <mannu.poski10@gmail.com>
2021-06-04 18:04:21 +0530

    Restructure test resources for `AppTest` (#67)

--- src/test/resources/update+insert/NEW_Lambda.java
+++
@@ -1,7 +0,0 @@
-import java.util.function.BiFunction;
-
-public class Lambda {
-    public static void main(String[] args) {
-        BiFunction<String, String, String> lambda = (x, y) -> { return y + " "; };
-    }
-}
--- src/test/resources/update+insert/PREV_Lambda.java
+++
@@ -1,7 +0,0 @@
-import java.util.function.Function;
-
-public class Lambda {
-    public static void main(String[] args) {
-        Function<String, String> lambda = (y) -> { return y + " "; };
-    }
-}
