5683229ee12d478d6640e4bc183586a9f06d7a27
Aman Sharma <mannu.poski10@gmail.com>
2021-06-21 21:18:12 +0530

    Incorporate more entities where final can be inserted

--- src/test/resources/insert/final/NEW_FinalModifier.java
+++ src/test/resources/insert/final/NEW_FinalModifier.java
@@ -1,5 +1,7 @@
-class FinalModifier {
+final class FinalModifier {
     public final int x;
     protected final int y;
     private final int z;
+
+    public final void doNothing() { }
 }
--- src/test/resources/insert/final/PREV_FinalModifier.java
+++ src/test/resources/insert/final/PREV_FinalModifier.java
@@ -2,4 +2,6 @@ class FinalModifier {
     public int x;
     protected int y;
     private int z;
+
+    public void doNothing() { }
 }
